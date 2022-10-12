d9f793e7a890c0ce4556f37f67ef6d515616198c
maky0204 <maky0204@hotmail.com>
2022-10-12 16:19:43 -0300

    Modificacion Index y agregado de js para filtrar

--- scripts/script0.js
+++ scripts/script0.js
@@ -1,1 +1,40 @@
 // Script Inicial ( vacio por ahora )
+
+function seleccion(){
+    if(document.getElementById('todos').checked){
+        console.log('Apreto Todos');
+    }
+    if(document.getElementById('drama').checked){
+        console.log('Apreto Drama');
+    }
+    if(document.getElementById('suspenso').checked){
+        console.log('Apreto suspenso');
+    }
+    if(document.getElementById('comedia').checked){
+        console.log('Apreto comedia');
+    }
+    if(document.getElementById('infantil').checked){
+        console.log('Apreto infantil');
+    }
+}
+
+
+/*
+document.querySelectorAll(".menuSolapas").forEach(el => {
+    el.addEventListener("menuSolapas", e => {
+      const codigo = e.target.getAttribute("href");
+      console.log("Se ha clickeado el id "+codigo);
+    });
+  });
+*/
+  
+
+function solapas(){
+    if(document.getElementsByName('menuTab').values){
+        var codigo = document.getElementsByClassName("menuSolapas")[0].id;
+        console.log(codigo);
+    }
+}
+
+
+
